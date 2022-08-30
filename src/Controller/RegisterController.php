bea7495edbffb103b4db2efcdad9b605760c9152
Badreddine <79919096+badreddine1987@users.noreply.github.com>
2022-08-30 12:31:09 +0200

    Add register action
    enregistrement

--- src/Controller/RegisterController.php
+++ src/Controller/RegisterController.php
@@ -2,12 +2,15 @@
 
 namespace App\Controller;
 
+use DateTime;
+use App\Entity\User;
+use App\Form\RegisterFormType;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasher;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 
 class RegisterController extends AbstractController
@@ -15,8 +18,40 @@ class RegisterController extends AbstractController
     #[Route('/inscription', name: 'user_register', methods: ['GET', 'POST'])]
     public function register(Request $request, EntityManagerInterface $entityManager, UserPasswordHasherInterface $passwordHasher): Response
     {
-        // return $this->render('register/index.html.twig', [
-        //     'controller_name' => 'RegisterController',
-        // ]);
+
+        // 1 - Instanciation
+
+        $user = new User();
+
+        // 2 - Creation du formulaire + mécanisme d'auto-hydratation
+
+        $form = $this->createForm(RegisterFormType::class, $user);
+        $form->handleRequest($request);
+
+        // 4 - au clic du bouton valider
+        if($form->isSubmitted() && $form->isValid()){
+
+            # set des proprietés qui ne sont pas dans le formulaire
+            $user->setCreatedAt(new DateTime());
+            $user->setUpdatedAt(new DateTime());
+            # La propriétées "roles" est une array []
+            $user->setRoles(['ROLE_USER']);
+
+            // 5 - hash de mots de passe manuellement
+            $user->setPassword(
+                $passwordHasher->hashPassword(
+                    $user, $form->get('password')->getData()
+                    )
+            );
+            # mettre en BDD
+            $entityManager->persist($user);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('default_home');
+        }
+        // 3- rendu de la vue Twig, avec le formulaire + createdVieuw() pour generer le HTML
+        return $this->render('register/form.html.twig', [
+            'form' => $form->createView() 
+        ]);
     }
 }
--- src/Form/RegisterFormType.php
+++ src/Form/RegisterFormType.php
@@ -6,24 +6,47 @@ use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 
 class RegisterFormType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('email')
-            // ->add('roles') rien n'a faire dans un formulaire
-            ->add('password')
-            ->add('firstname')
-            ->add('lastname')
-            ->add('gender')
-            ->add('submit', SubmitType::class, [
-                'label' => 'valider'
-            
+            ->add('email', EmailType::class, [
+                'label' => 'Email',
             ])
-                // ne doivent jamais apparaitre dans le formulaire
+            // ->add('roles') rien n'a faire dans un formulaire
+            ->add('password', PasswordType::class, [
+                'label' => 'Mots de passe',
+            ])
+            ->add('firstname', TextType::class, [
+                'label' => 'Prénom'
+            ])
+            ->add('lastname', TextType::class, [
+                'label' => 'Nom'
+            ])
+            ->add('gender', ChoiceType::class, [
+                'label' => 'Civilité',
+                'expanded' => true,
+                'choices' => [
+                    'Homme' => 'homme',
+                    'Femme' => 'femme'
+                ],
+            ])
+            ->add('submit', SubmitType::class, [
+                'label' => 'valider',
+                'validate' => false,
+                'attr' => [
+                    'class' => 'd-block mx-auto btn btn-warning col-4'
+                    ]
+
+            ])
+            // ne doivent jamais apparaitre dans le formulaire
             // ->add('createdAt')
             // ->add('updatedAt')
             // ->add('deletedAt')
