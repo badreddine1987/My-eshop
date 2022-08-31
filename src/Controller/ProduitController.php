03468b652dca1155302dc44725c72d65a02da832
Badreddine <79919096+badreddine1987@users.noreply.github.com>
2022-08-31 16:54:42 +0200

    Add create function for produit + handle file to upload

--- src/Controller/ProduitController.php
+++ src/Controller/ProduitController.php
@@ -11,7 +11,8 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-
+use Symfony\Component\HttpFoundation\File\Exception\FileException;
+use Symfony\Component\String\Slugger\SluggerInterface;
 
 #[Route("/admin")]
 class ProduitController extends AbstractController
@@ -33,8 +34,8 @@ class ProduitController extends AbstractController
     # 4 - Créer le fichier Twig de cette vue.
     # 5 - Finir la partie POST dans l'action.
 
-    #[Route('/Ajouter un produit', name: 'create_produit', methods: ['GET', 'POST'])]
-    public function createProduit(Request $request, EntityManagerInterface $entityManager): Response
+    #[Route('/Ajouter-un-produit', name: 'create_produit', methods: ['GET', 'POST'])]
+    public function createProduit(Request $request, EntityManagerInterface $entityManager, SluggerInterface $slugger): Response
     {
         $produit = new Produit();
 
@@ -46,11 +47,49 @@ class ProduitController extends AbstractController
                     $produit->setCreatedAt(new DateTime());
                     $produit->setUpdatedAt(new DateTime());
 
+
+                    $photo = $form->get('photo')->getData();
+
+                    if($photo){
+
+                        # 1 - Déconstruire le nom du fichier
+
+                        # a - On récupère l'extension grâce à la méthode guessExtension()
+                            $extension = '.' . $photo->guessExtension();
+                        # 2 - Sécuriser le nom et le reconstruire le nouveau nom du fichier
+                        # a - On assainit le nom du fichier pour suprimer les espaces et les accents
+                            $safeFilename = $slugger->slug($photo->getClientOriginalName());
+
+                        # b - On reconstruit le nom du fichier
+                        # uniqid() est une fonction native de PHP qui genere un identifiant unique
+                        $newFilename = $safeFilename . '_' . uniqid() . $extension;
+
+
+                        # 3 - Déplacer le fichier dans le bon dossier  
+                        # try/catch
+
+                            try {
+
+                                    $photo->move($this->getParameter('uploads_dir'), $newFilename);
+                                    $produit->setPhoto($newFilename);
+
+                            }catch(FileException $exception) {
+                                $this->addFlash('warning', 'La photo du produit ne s\'est pas importée avec succès. Veuillez réessayer en modifiant le produit.');
+
+                          //      $exception->setMessage();
+                                return $this->redirectToRoute('create_produit');
+
+                            }
+                    } // end if photo
+
+
                     $entityManager->persist($produit);
                     $entityManager->flush();
 
-                    return $this->redirectToRoute('default_home');
-            }
+                    $this->addFlash('success', 'Produit ajouter avec succes');
+
+                    return $this->redirectToRoute('show_produits');
+            } // end form
 
         return $this->render('admin/produit/create_produits.html.twig', [
             'form_produit' => $form->createView(),
--- src/Form/ProduitFormType.php
+++ src/Form/ProduitFormType.php
@@ -6,6 +6,9 @@ use App\Entity\Produit;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 
 class ProduitFormType extends AbstractType
@@ -13,16 +16,48 @@ class ProduitFormType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('title')
-            ->add('description')
-            ->add('color')
-            ->add('size')
-            ->add('gender')
-            ->add('photo')
-            ->add('price')
-            ->add('stock')
+            ->add('title', TextType::class, [
+                'label' => 'Titre',
+            ])
+            ->add('description', TextType::class, [
+                'label' => 'Description',
+            ])
+            ->add('color', TextType::class, [
+                'label' => 'Couleur',
+            ])
+            ->add('size', ChoiceType::class, [
+                'label' => 'Taille',
+                'choices' => [
+                    'S' => 's',
+                    'M' => 'm',
+                    'L' => 'l',
+                    'XL' => 'xl',
+                    'XXL' => 'xxl',
+                ]
+            ])
+            ->add('gender', ChoiceType::class, [
+                'label' => 'Sexe',
+                'choices' => [
+                    'Homme' => 'homme',
+                    'Femme' => 'femme'
+                ]
+            ])
+            ->add('photo', FileType::class, [
+                'label' => 'Photo du produit',
+                
+            ])
+            ->add('price', TextType::class, [
+                'label' => 'Prix',
+            ])
+            ->add('stock', TextType::class, [
+                'label' => 'Quantité',
+            ])
             ->add('submit', SubmitType::class, [
-                'label' => 'Valider'
+                'label' => 'Ajouter produit',
+                'validate' => false,
+                'attr' => [
+                    'class' => 'd-block mx-auto btn btn-warning col-4'
+                    ]
             ])
         ;
     }
@@ -31,6 +66,8 @@ class ProduitFormType extends AbstractType
     {
         $resolver->setDefaults([
             'data_class' => Produit::class,
+            'allow_file_upload' => true,
+            'photo' => null,
         ]);
     }
 }
